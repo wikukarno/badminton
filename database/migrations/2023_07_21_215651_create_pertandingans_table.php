1b11d2c800822ccd80b48a6a778f04cb02111826
Rinaldi Pratama Putra <rinaldiprtma@gmail.com>
2023-07-21 22:04:17 +0700

    Tambah database migration pertandingan

---
+++ database/migrations/2023_07_21_215651_create_pertandingans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pertandingans', function (Blueprint $table) {
+            $table->id();
+            $table->string('perlombaans_id', 12);
+            $table->string('pesertas_id_1', 12);
+            $table->string('pesertas_id_2', 12);
+            $table->dateTime('tanggal_jadwal');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pertandingans');
+    }
+};
